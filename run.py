2b05cdcfc82ed8ecc0c283d8b09d2ad5291dc277
Gleb Mezentsev <gleb.mezentsev@skoltech.ru>
2021-09-02 09:38:37 +0000

    reproduce results after refactoring

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 import torch.nn as nn
 import wandb
@@ -5,20 +7,25 @@ import wandb
 from model import NODESolver
 from data import DataGenerator
 from train import train
+from callbacks import ensure_clean_worktree, get_commit_hash
 
 
 if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--dev", help="running in dev mode", action="store_true")
+  args = parser.parse_args()
+
   wandb.login()
 
   device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-  training_params = {'lambd': 0.2, 'n_iter': 10000, 'n_batch_steps': 1, 'lr':1e-3}
+  training_params = {'lambd': 0.2, 'n_iter': 10000, 'n_batch_steps': 1, 'lr': 1e-3}
 
   data_params = {'latent_dim': 5, 'signal_dim': 1,
-                'trajectory_len': 200, 'batch_size': 32, 'signal_max_amp':3,
-                'signal_t_min': 0, 'signal_t_max': 4*3.14, 'signal_noise_amp':0.2,
-                'rand_p':3, 'rand_q':0, 'rand_max_amp':0, 'rand_noise_amp':0}
+                'trajectory_len': 200, 'batch_size': 32, 'signal_max_amp': 3,
+                'signal_t_min': 0, 'signal_t_max': 4*3.14, 'signal_noise_amp': 0.2,
+                'rand_p': 3, 'rand_q': 0, 'rand_max_amp': 1, 'rand_noise_amp': 0.2}
 
   model_params = {'encoder_n_layers': 3, 'encoder_hidden_channels': 5,
                   'decoder_n_layers': 3, 'decoder_hidden_dim': 5,
@@ -39,11 +46,22 @@ if __name__ == '__main__':
   config = {**training_params, **data_params, **model_params, 'opimizer': optimizer.__class__.__name__,
             'node_criterion': node_criterion.__class__.__name__, 'rec_criterion': rec_criterion.__class__.__name__}
 
+  if args.dev:
+    config['n_iter'] = 1
+    mode = 'offline'
+
+  else:
+    ensure_clean_worktree()
+    mode = 'online'  
+
+  experiment_name = 'reproduce results'
+
   wandb.init(project='Sinus approximation',
-            notes='testing',
-            tags=['noiseless', 'testing'],
-            config=config)
-
+              notes='testing',
+              tags=[],
+              config=config,
+              name=experiment_name,
+              mode=mode)
+  
   wandb.watch(model)
-
-train(model, optimizer, data_generator, node_criterion, rec_criterion, device, config)
+  train(model, optimizer, data_generator, node_criterion, rec_criterion, device, config)
