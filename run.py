c0fd32190926753db25568411c38823e1175e2a7
Gleb Mezentsev <gleb.mezentsev@skoltech.ru>
2021-10-12 14:53:41 +0300

    RoFormer Spiral

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import argparse
 
 import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"  
-os.environ["CUDA_VISIBLE_DEVICES"]="6"
+os.environ["CUDA_VISIBLE_DEVICES"]="5"
 
 import torch
 import torch.nn as nn
@@ -29,7 +29,7 @@ random.seed(seed)
 
 
 
-DATASET = 'SIN'
+DATASET = 'SPIRAL'
 
 
 
@@ -60,11 +60,11 @@ if __name__ == '__main__':
   elif DATASET == 'SPIRAL':
 
     data_params = {'latent_dim': 5, 'signal_dim': 2,
-                  'trajectory_len': 200, 'batch_size': 1024, 'signal_max_amp': 2,
+                  'trajectory_len': 200, 'batch_size': 256, 'signal_max_amp': 2,
                   'signal_t_min': 0, 'signal_t_max': 20, 'signal_noise_amp': 0.2,
                   'rand_p': 3, 'rand_q': 0, 'rand_max_amp': 1, 'rand_noise_amp': 0.2}
 
-    model_params = {'encoder_n_layers': 3, 'encoder_hidden_channels': 5,
+    model_params = {'encoder_n_layers': 3, 'encoder_hidden_channels': 256,
                     'decoder_n_layers': 3, 'decoder_hidden_dim': 5,
                     'rhs_n_layers': 3, 'rhs_hidden_dim': 5}
 
@@ -111,7 +111,7 @@ if __name__ == '__main__':
     ensure_clean_worktree()
     mode = 'online'  
 
-  experiment_name = 'RoFormer encoder Sin'
+  experiment_name = 'RoFormer encoder SPIRAL'
 
   wandb.init(project='Sinus approximation',
               notes='',
