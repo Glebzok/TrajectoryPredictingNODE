c7c595236ef9232ae9e82908e5e23ab19f7b99e9
Gleb Mezentsev <gleb.mezentsev@skoltech.ru>
2021-10-15 13:44:33 +0300

    RoFormer smaller net, larger lr sin

--- run.sh
+++ run.sh
@@ -4,12 +4,15 @@
 COMMIT=$(git rev-parse --short HEAD)
 
 MODE=$1
+DEVICE=$2
+DATASET=$3
+EXPERIMENT="$4"
 
 if [ $MODE == "DEV" ]
 then 
-    COMMAND="cd ../NeuralODE && pip install -r requirements.txt && wandb login 7aedec3427d399660c93a0ac0d61da08f861325b && python run.py -device $2 -dataset $3 -experiment $4 --dev"
+    COMMAND="cd ../NeuralODE && pip install -r requirements.txt && wandb login 7aedec3427d399660c93a0ac0d61da08f861325b && python run.py -device ${DEVICE} -dataset ${DATASET} -experiment ${EXPERIMENT} --dev"
 else 
-    COMMAND="cd ../NeuralODE && pip install -r requirements.txt && wandb login 7aedec3427d399660c93a0ac0d61da08f861325b && python run.py -device $2 -dataset $3 -experiment $4"
+    COMMAND="cd ../NeuralODE && pip install -r requirements.txt && wandb login 7aedec3427d399660c93a0ac0d61da08f861325b && python run.py -device ${DEVICE} -dataset ${DATASET} -experiment ${EXPERIMENT}"
 fi
 
 nvidia-docker run --rm -v /raid/data/gmezentsev/mycode/NeuralODE:/NeuralODE \
