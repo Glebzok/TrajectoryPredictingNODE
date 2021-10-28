8ab65a527ce47f61d270cb4810ad7d5f2a5d2230
Gleb Mezentsev <gleb.mezentsev@skoltech.ru>
2021-10-28 09:55:57 +0300

    0.2 noise sin

--- run.py
+++ run.py
@@ -49,8 +49,8 @@ if __name__ == '__main__':
 
     data_params = {'latent_dim': 5, 'signal_dim': 1,
                   'trajectory_len': 200, 'batch_size': 256, 'signal_max_amp': 3,
-                  'signal_t_min': 0, 'signal_t_max': 4*3.14, 'signal_noise_amp': 0.1,
-                  'rand_p': 3, 'rand_q': 0, 'rand_max_amp': 1, 'rand_noise_amp': 0.1}
+                  'signal_t_min': 0, 'signal_t_max': 4*3.14, 'signal_noise_amp': 0.2,
+                  'rand_p': 3, 'rand_q': 0, 'rand_max_amp': 1, 'rand_noise_amp': 0.2}
 
     model_params = {'encoder_n_layers': 3, 'encoder_hidden_channels': 64,
                     'decoder_n_layers': 3, 'decoder_hidden_dim': 5,
